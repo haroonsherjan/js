0c71546f1ef469e68d535a4cf8160c7549538c64
Haroon <haroonsherjan@gmail.com>
2021-02-02 16:55:55 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//document.getElementById()
+document.getElementById('task-title').style.background = '#333'
+document.getElementById('task-title').style.color = '#fff'
+// document.getElementById('task-title').textContent = 'Task List';
+document.getElementById('task-title').innerText = 'My Tasks';
+
+console.log('hello');
+
+document.querySelector('li').style.color = 'red'
+
+let items = document.getElementsByClassName('collection-item');
+console.log(items)
+items[3].style.color = 'red'
